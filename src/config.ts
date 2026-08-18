da77392ca9e1a90aa7f4b97eba309f2d73b9f0b0
Jose <negrolo.4@gmail.com>
2026-08-18 13:43:49 +0300

    quita link de LinkedIn placeholder del footer

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const TELEGRAM_URL = 'https://t.me/dominickrex_bot';
 export const WEB3FORMS_KEY = '88d54eb5-1cd0-47c0-9d2a-bc583646cf68';
 export const CONTACT_EMAIL = 'info@axiomadigital.tech';
-export const LINKEDIN_URL = 'https://www.linkedin.com/in/TU_PERFIL';
+// LINKEDIN_URL: pendiente, se agrega cuando haya perfil real
 export const PROJECTS = {
   kalkulos: 'https://kalkulos.com',
   nexum: 'https://nexumgroup.work',
